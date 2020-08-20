067a3dd3c77ba71e9b87c657d8efbf641c97f7a7
Alexey <valnistuy@gmail.com>
2020-08-20 23:24:38 +0300

    :docs: added logout logics

--- src/components/LoginTest/LoginTest.js
+++ src/components/LoginTest/LoginTest.js
@@ -10,20 +10,19 @@ const LoginTest = () => {
     confirmPassword: "",
     name: "",
   });
+  const [userTokenG, setUserTokenG] = useState("");
+  const [userTokenF, setUserTokenF] = useState("");
 
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    console.log(form);
-
     try {
-      const newUser = await axios.post(
-        // "https://warm-reef-14893.herokuapp.com/api/v1/auth/register",
+      const { data } = await axios.post(
         "http://localhost:4242/auth/register",
         form
       );
 
-      console.log(newUser.data);
+      console.log(data);
     } catch (error) {
       console.error(error);
     }
@@ -38,103 +37,85 @@ const LoginTest = () => {
   };
 
   const responseGoogle = async (response) => {
-    const { accessToken } = response;
+    try {
+      const { accessToken } = response;
 
-    const body = {
-      accessToken,
-    };
+      const body = {
+        accessToken,
+      };
 
-    const bla = await axios.post(
-      // "https://warm-reef-14893.herokuapp.com/api/v1/auth/google",
-      // "http://localhost:4242/api/v1/auth/google",
-      "http://localhost:4242/auth/google",
-      body
-    );
+      const { data } = await axios.post(
+        "http://localhost:4242/auth/google",
+        body
+      );
 
-    console.log(bla);
+      setUserTokenG(data.token);
+      setUserTokenF("");
+
+      console.log("GOOGLE LOGIN GOOD");
+      console.log(data);
+    } catch (error) {
+      console.log("GOOGLE LOGIN FAILED");
+      console.error(error);
+    }
   };
 
   const responseFacebook = async (response) => {
-    const { accessToken } = response;
+    try {
+      const { accessToken } = response;
 
-    console.log(accessToken);
+      const body = {
+        accessToken,
+      };
 
-    const body = {
-      accessToken,
-    };
+      const { data } = await axios.post(
+        "http://localhost:4242/auth/facebook",
+        body
+      );
 
-    const bla = await axios.post(
-      // "https://warm-reef-14893.herokuapp.com/api/v1/auth/facebook",
-      // "http://localhost:4242/api/v1/auth/facebook",
-      "http://localhost:4242/auth/facebook",
-      body
-    );
+      setUserTokenF(data.token);
+      setUserTokenG("");
 
-    console.log(bla);
+      console.log("FACEBOOK LOGIN GOOD");
+
+      console.log(data);
+    } catch (error) {
+      console.log("FACEBOOK LOGIN FAILED");
+      console.error(error);
+    }
   };
 
-  const handleGoogleAuth = async () => {
-    // await axios.get("https://accounts.google.com/o/oauth2/v2/auth");
-    const params = new URLSearchParams({
-      client_id:
-        "566572400240-1bqcgkac9k0v7j836crur44omj0htaoi.apps.googleusercontent.com",
-      redirect_uri: "https://warm-reef-14893.herokuapp.com/api/v1/auth/google",
-      response_type: "code",
-      scope: "openid email profile",
-    });
+  const userLogout = async () => {
+    let bearerToken = null;
 
-    const url =
-      "https://accounts.google.com/o/oauth2/v2/auth?" + params.toString();
+    if (userTokenG) {
+      bearerToken = userTokenG;
+    } else if (userTokenF) {
+      bearerToken = userTokenF;
+    }
 
-    // const test = window.open(url);
+    try {
+      const opt = {
+        method: "POST",
+        url: "http://localhost:4242/auth/logout",
+        headers: {
+          Authorization: `Bearer ${bearerToken}`,
+          "Content-Type": "application/x-www-form-urlencoded",
+        },
+      };
+      const res = await axios(opt);
 
-    let style = `scrollbars=no,resizable=no,status=no,location=no,toolbar=no,menubar=no,
-width=600,height=300,left=100,top=100`;
+      console.log("LOGOUT GOOD");
 
-    // const bla = await axios.get(url);
-    // console.log(bla);
-
-    window.open(url, "test", style);
-
-    // const bla = await axios.get(
-    //   "https://warm-reef-14893.herokuapp.com/api/v1/auth/google",
-    //   params
-    // );
-    // console.log(bla);
-  };
-
-  const handleFacebookAuth = () => {
-    const params = new URLSearchParams({
-      client_id: "641950426421786",
-      redirect_uri:
-        "https://warm-reef-14893.herokuapp.com/api/v1/auth/facebook",
-      // state: "{st=state123abc,ds=123456789}",
-      scope: ["email", "user_friends"].join(","),
-      response_type: "code",
-      auth_type: "rerequest",
-      display: "popup",
-    });
-
-    console.log(params.toString());
-
-    const url =
-      "https://www.facebook.com/v8.0/dialog/oauth?" + params.toString();
-
-    let style = `scrollbars=no,resizable=no,status=no,location=no,toolbar=no,menubar=no,
-width=600,height=300,left=100,top=100`;
-
-    const newWin = window.open(url, "test", style);
-
-    if (newWin.opener) {
-      // send them to the opening window
-      newWin.opener.postMessage(params);
-      // close the popup
-      newWin.close();
+      console.log(res);
+    } catch (error) {
+      console.log("LOGOUT FAILED");
+      console.log(error);
     }
   };
 
   return (
-    <>
+    <div>
       <form onSubmit={handleSubmit}>
         <div>
           <input
@@ -174,12 +155,7 @@ width=600,height=300,left=100,top=100`;
         </div>
         <button type="submit">register</button>
       </form>
-      <div>
-        <button onClick={handleGoogleAuth}>Google o_auth</button>
-      </div>
-      <div>
-        <button onClick={handleFacebookAuth}>Facebook o_auth</button>
-      </div>
+      <br />
       <div>
         <GoogleLogin
           clientId="1016113227604-0u7tph6hm9s2bbnvi19h8dvdg3h5lqu4.apps.googleusercontent.com"
@@ -187,18 +163,24 @@ width=600,height=300,left=100,top=100`;
           onSuccess={responseGoogle}
           onFailure={responseGoogle}
           autoLoad={false}
-          // cookiePolicy={"single_host_origin"}
         />
-      </div>
-      <div>
         <FacebookLogin
           appId="628094021455729"
           fields="name,email,picture"
-          // onClick={componentClicked}
           callback={responseFacebook}
+          autoLoad={false}
+          buttonStyle={{
+            padding: "12px 16px",
+            fontSize: "14px",
+            marginLeft: "10px",
+          }}
         />
       </div>
-    </>
+      <br />
+      <div>
+        <button onClick={userLogout}>logout</button>
+      </div>
+    </div>
   );
 };
 
