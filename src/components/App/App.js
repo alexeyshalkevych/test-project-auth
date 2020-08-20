49b1dce3fdbfadd5af24f0f20d20025fe6d23058
Alexey <valnistuy@gmail.com>
2020-08-20 19:54:21 +0300

    :refactor: change gh-pages

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,9 +1,11 @@
 import React from "react";
+import LoginTest from "../LoginTest/LoginTest";
 
 const App = () => {
   return (
     <div>
       <h2>Hello Github</h2>
+      <LoginTest />
     </div>
   );
 };
